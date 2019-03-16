f08fb7f1aa4edaa8fa900771b54b612416414723
Son Truong <sntruong12@gmail.com>
2019-03-16 14:29:31 -0500

    initial set up for models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Set(models.Model):
+  name = models.CharField(max_length=200)
+  subject = models.CharField(max_length=200)
+  description = models.TextField(max_length=500)
+
+  def __str__(self):
+    return self.name
+
+class Flashcard(models.Model):
+  question = models.TextField(max_length=500)
+  answer = models.TextField(max_length=500)
+  
+  def __str__(self):
+    return f"""question: {self.question}
+      answer: {self.answer}"""
+
+class Note(models.Model):
+  content = models.TextField()
+
+  def __str__(self):
+    return self.content
+
+class Comment(models.Model):
+  content = models.TextField(max_length=500)
+  timestamp = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.content
