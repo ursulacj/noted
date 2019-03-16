c97dd6302249ccc842989b72c006c3634a3059d2
ursulacj <ursulacedillojohnson@gmail.com>
2019-03-16 17:48:48 -0500

    added more nav functionality

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -3,4 +3,6 @@ M.AutoInit();
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems, options);
+    var sideNav = document.querySelectorAll('.sidenav');
+    var navInstances = M.Sidenav.init(elems, options);
 });
