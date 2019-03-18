e6ef8ac76adb896aaba938ab0e103a4a204452f3
ursulacj <ursulacedillojohnson@gmail.com>
2019-03-18 13:00:27 -0500

    added login_required and LoginRequiredMixin to views

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Set, Flashcard
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Set, Flashcard
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import FlashcardForm
 from .forms import FlashcardForm, SetFlashcardFormSet
 
@@ -11,14 +15,6 @@ from .forms import FlashcardForm, SetFlashcardFormSet
 
 def home(request):
   return render(request, 'home.html')
-
-def sets_index(request):
-  sets = Set.objects.filter(user=request.user)
-  return render(request, 'sets/index.html', { 'sets': sets } )
-
-def show_set(request, set_id):
-  set = Set.objects.get(id=set_id)
-  return render(request, 'sets/show.html', {'set': set } )
 
 def signup(request):
   error_message = ''
@@ -39,25 +35,39 @@ def signup(request):
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-class SetCreate(CreateView):
+@login_required
+def sets_index(request):
+  sets = Set.objects.filter(user=request.user)
+  return render(request, 'sets/index.html', { 'sets': sets } )
+
+@login_required
+def show_set(request, set_id):
+  set = Set.objects.get(id=set_id)
+  return render(request, 'sets/show.html', {'set': set } )
+
+
+class SetCreate(LoginRequiredMixin, CreateView):
   model = Set
   fields = '__all__'
   success_url = '/sets/'
 
-class SetUpdate(UpdateView):
+class SetUpdate(LoginRequiredMixin, UpdateView):
   model = Set
   fields = '__all__'
 
-class SetDelete(DeleteView):
+class SetDelete(LoginRequiredMixin, DeleteView):
   model = Set
   success_url = '/sets/'
 
 # class FlashcardCreate(CreateView):
 #   form_class = FlashcardForm
 #   template_name = 'main_app/flashcard_form.html'
+
+@login_required
 def flashcards_index(request, set_id):
   pass
 
+@login_required
 def create_flashcards(request, set_id):
   set = Set.objects.get(id=set_id)
   return render(request, 'main_app/flashcard_form.html', {
