8e953937049ce7ac876de7c4792ec987cb7acf38
Son Truong <sntruong12@gmail.com>
2019-03-19 16:25:19 -0500

    add path for showing a group

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
   path('sets/<int:set_id>/flashcards/create/', views.create_flashcards, name='create_flashcards'),
   path('groups/', views.groups_index, name='groups_index'),
   path('groups/create', views.GroupCreate.as_view(), name='create_group'),
+  path('groups/<int:group_id>/', views.show_group, name='show_group'),
   path('contact_us/', views.contact_us, name='contact_us'),
   path('success/', views.successView, name='success'),
 ]
