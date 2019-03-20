24be6e482bd1b5cab0f91a9091a4f314f3de7e3b
Son Truong <sntruong12@gmail.com>
2019-03-20 12:59:43 -0500

    remove options from side nav and fix bug with parallax images not showing up on about page

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,11 +1,20 @@
+var navDrop = document.getElementsByClassName('.dropdown-trigger');
+M.Dropdown.init(navDrop, {
+    hover: true,
+    coverTrigger: false,
+    closeOnClick: false
+});
 
-    var navDrop = document.getElementsByClassName('.dropdown-trigger');
-    M.Dropdown.init(navDrop, {
-        hover: true,
-        coverTrigger: false,
-        closeOnClick: false
-    });
+var sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
 
-    var sideNav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sideNav, options);
-
+// initialize the parallax elements
+var parallaxEls = document.querySelectorAll('.parallax');
+function initParallax(instances) {
+    if (instances.length > 0) {
+        for(var i = 0; i < instances.length; i++) {
+            M.Parallax.init(instances[i]);
+        }
+    }
+}
+initParallax(parallaxEls)
