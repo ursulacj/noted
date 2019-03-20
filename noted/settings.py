e7e679930d0b761db2118562872c3b9becc88adf
ursulacj <ursulacedillojohnson@gmail.com>
2019-03-20 12:29:34 -0500

    configuring deployment

--- noted/settings.py
+++ noted/settings.py
@@ -119,3 +119,9 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 EMAIL_PORT = 1025
+
+# Other settings above
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
