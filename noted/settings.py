877694de92e3f4af7e11368c94fabc2e1f955ab5
ursulacj <ursulacedillojohnson@gmail.com>
2019-03-22 10:26:26 -0500

    added beautification to additional sets

--- noted/settings.py
+++ noted/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/sets/'
+LOGIN_REDIRECT_URL = '/accounts/my_account'
 LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
